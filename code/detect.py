00034b7d159a111160f9c6fade05e02acf01703b
GeRongGang <164444685+GeRongGang@users.noreply.github.com>
2025-01-04 21:13:00 +0800

    Add files via upload

---
+++ code/detect.py
@@ -0,0 +1,15 @@
+"""保存文件：带框的图片、特征图"""
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('runs/train/exp/weights/best.pt') # select your model.pt path
+    model.predict(source='dataset/images/test',
+                  imgsz=640,
+                  project='runs/detect',
+                  name='exp',
+                  save=True,
+                  # conf=0.2,
+                  # visualize=True # visualize model features maps 特征图
+                )
---
+++ code/main_profile.py
@@ -0,0 +1,15 @@
+"""查看模型参数量、计算量"""
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    # choose your yaml file
+    model = YOLO('ultralytics/cfg/models/v8/yolov8.yaml')
+    model.info(detailed=True)
+    try:
+        model.profile(imgsz=[640, 640])
+    except Exception as e:
+        print(e)
+        pass
+    model.fuse()
